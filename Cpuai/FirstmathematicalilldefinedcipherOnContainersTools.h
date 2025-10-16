941fdb40ebc7069e5eeb7aeb9bbf4cfa62f5cc48
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-16 06:24:34 +0200

    Create FirstmathematicalilldefinedcipherOnContainersTools.h

---
+++ Cpuai/FirstmathematicalilldefinedcipherOnContainersTools.h
@@ -0,0 +1,12 @@
+
+//the tools need cronbach values on ui/ux results of neiji93 repositories.
+//the log function will help us to stop cpu ai cached.
+//it means rhat we dont deal with high energy devices and bots.
+
+
+//we start and define our firet class without ai because in computer vision we still need right bottom
+//corner of camera images to get diffuse scattering. according to my stuffs, it is good to keep track
+//of hear blocks internally. then, we can stop intermediate devices heat blocks stuffs. pous, graohics are 
+//stopped at the same time in one step or 0,06 step. so, the heat rransfer computed internally without cpu ai cafhe is wrong
+//because high energy devices dont start at 15 keV.
+class oncpuaisforonestepWithoutPlanckconstantandwithoutai
