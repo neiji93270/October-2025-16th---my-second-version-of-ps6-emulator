b34a59c311b94582e077bd5ad5a8632b342baf87
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-16 12:06:15 +0200

    Create Largesxaleusbbootable.h

---
+++ Bootusb/Largesxaleusbbootable.h
@@ -0,0 +1,6 @@
+//as we can have keV compo'ents and as we can drop
+//several text words on protocols, we can use a large scale usb boot
+//to prevent intermediate component programs ro merge with other programs
+//if tensors use at least corpus algebras or ring algebras.
+//it means that we have only one case of treshold in low level energy compo'e'ts.
+//then, we can use usb eletronics. notice that this behaviour is unlilely to drop ciphers stuffs.
